7a1859971e73a790e203f9a6b08344ad224c1201
Iman Maryam Morongwa Ngwepe-Ntshibida <108999761+Iam-Iman@users.noreply.github.com>
2023-02-22 18:08:28 +0000

    added form entries to db

--- database.py
+++ database.py
@@ -27,8 +27,7 @@ def load_jobs_from_db():
 def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
-      text('select * from jobs where id = :val'),
-      val=id    
+      text(f"select * from jobs where id = {id}")   
       )
     rows = result.all()
     if len(rows) == 0:
@@ -36,6 +35,19 @@ def load_job_from_db(id):
     else:
       return rows[0]._asdict()
 
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query,
+                 job_id=data['job_id'],
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url']
+                )
   #print("type(result):", type(result))
   #result_all = result.all()
   #print("type(result.all):", type(result_all))
